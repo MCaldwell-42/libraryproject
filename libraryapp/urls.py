b5033285c0fd596b9e76b8e09df7a40aa00611e9
Matthew Caldwell <MCaldwell4242@gmail.com>
2019-09-19 13:46:30 -0500

    libraries adding

--- libraryapp/urls.py
+++ libraryapp/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     url(r'^libraries$', library_list, name='libraries'),
     url(r'accounts/', include('django.contrib.auth.urls')),
     url(r'^logout/$', logout_user, name='logout'),
+    url(r'^books/form$', book_form, name='book_form'),
+    url(r'^libraries/form$', library_form, name='library_form'),
 ]
